a07629dc6861e5f08255f93af14a2383b693aa53
Robin ABELA <abelar@iut-acy.local>
2025-03-28 16:51:46 +0100

    pudate

---
+++ lib/states/auth_state.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:event_poll/configs.dart';
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+
+import 'package:http/http.dart' as http;
+
+class AuthState extends ChangeNotifier{
+
+
+  User? _currentUser;
+  String? _token;
+
+  User? get currentUser => _currentUser;
+  String? get token => _token;
+  bool get isLoggedIn => _currentUser == null;
+
+  Future<User?> login(String username, String password) async {
+  final loginResponse = await http.post(
+    Uri.parse('${Configs.baseUrl}/auth/login'),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+    body: json.encode({
+      'username': username,
+      'password': password,
+    }),
+  );
+
+  if (loginResponse.statusCode == HttpStatus.ok) {
+    _token = json.decode(loginResponse.body)['token'];
+
+    final userResponse = await http.get(
+      Uri.parse('${Configs.baseUrl}/users/me'),
+      headers: {
+        HttpHeaders.authorizationHeader: 'Bearer $_token',
+        HttpHeaders.contentTypeHeader: 'application/json',
+      },
+    );
+
+    if (userResponse.statusCode == HttpStatus.ok) {
+      _currentUser = User.fromJson(json.decode(userResponse.body));
+      notifyListeners();
+      return _currentUser;
+    }
+  }
+  void logout() {
+    _token = null;
+    _currentUser =null;
+    notifyListeners();
+  }
+
+  logout();
+
+
+
+  
+
+  return null;
+}
+
+}
